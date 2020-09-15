0029e7ae5a35028739a96ddc977ef381a8439f4d
mittranj8 <mj4mittranj@gmail.com>
2020-09-15 18:15:24 -0500

    added radio buttons

--- covid19-case-tracker-app/src/app/components/home/home.component.ts
+++ covid19-case-tracker-app/src/app/components/home/home.component.ts
@@ -26,15 +26,46 @@ export class HomeComponent implements OnInit {
 
   constructor(private dataService: DataServiceService) {}
 
-  initChart() {
+  ngOnInit(): void {
+    this.dataService.getGlobalData().subscribe({
+      next: (result) => {
+        console.log(result);
+        this.globalData = result;
+
+        // displaying all cases in the component
+        result.forEach((cs) => {
+          if (!Number.isNaN(cs.confirmed)) {
+            this.totalActive += cs.active;
+            this.totalConfirmed += cs.confirmed;
+            this.totalDeaths += cs.deaths;
+            this.totalRecovered += cs.recovered;
+          }
+        });
+        this.initChart("c");
+      },
+    });
+  }
+
+  updateChart(input: HTMLInputElement) {
+    this.initChart(input.value);
+  }
+
+  initChart(caseType: string) {
     let tableData = [];
     tableData.push(["Country", "Cases"]);
 
     this.globalData.forEach((cs) => {
-      // display countries with more than 2000 cases
-      if (cs.confirmed > 2000) {
-        tableData.push([cs.country, cs.confirmed]);
-      }
+      let value: number;
+      // returning values based on selected case type with countries with more than 2000 cases
+      if (caseType == "c") if (cs.confirmed > 2000) value = cs.confirmed;
+
+      if (caseType == "r") if (cs.recovered > 2000) value = cs.recovered;
+
+      if (caseType == "d") if (cs.deaths > 2000) value = cs.deaths;
+
+      if (caseType == "a") if (cs.active > 2000) value = cs.active;
+
+      tableData.push([cs.country, value]);
     });
 
     this.pieChart = {
@@ -52,25 +83,5 @@ export class HomeComponent implements OnInit {
         height: 500,
       },
     };
-  }
-
-  ngOnInit(): void {
-    this.dataService.getGlobalData().subscribe({
-      next: (result) => {
-        console.log(result);
-        this.globalData = result;
-
-        // displaying all cases in the component
-        result.forEach((cs) => {
-          if (!Number.isNaN(cs.confirmed)) {
-            this.totalActive += cs.active;
-            this.totalConfirmed += cs.confirmed;
-            this.totalDeaths += cs.deaths;
-            this.totalRecovered += cs.recovered;
-          }
-        });
-        this.initChart();
-      },
-    });
   }
 }
