b73561773f779bdb204af1f9a45caf33b6c91769
mittranj8 <mj4mittranj@gmail.com>
2020-09-15 15:56:38 -0500

    displaying country names

--- covid19-case-tracker-app/src/app/components/countries/countries.component.ts
+++ covid19-case-tracker-app/src/app/components/countries/countries.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { GlobalDataSummary } from "src/app/models/global-data";
+import { DataServiceService } from "src/app/services/data-service.service";
 
 @Component({
-  selector: 'app-countries',
-  templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.css']
+  selector: "app-countries",
+  templateUrl: "./countries.component.html",
+  styleUrls: ["./countries.component.css"],
 })
 export class CountriesComponent implements OnInit {
-
-  constructor() { }
+  data: GlobalDataSummary[];
+  countries: string[] = [];
+  constructor(private dataService: DataServiceService) {}
 
   ngOnInit(): void {
+    this.dataService.getGlobalData().subscribe((result) => {
+      this.data = result;
+      this.data.forEach((cs) => {
+        this.countries.push(cs.country);
+      });
+    });
   }
-
 }
