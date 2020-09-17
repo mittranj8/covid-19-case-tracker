a5d9131e48bbe9dd928050ff22b2f5b8827942d4
mittranj8 <mj4mittranj@gmail.com>
2020-09-17 03:03:26 -0500

    added new and wild routes

--- covid19-case-tracker-app/src/app/app-routing.module.ts
+++ covid19-case-tracker-app/src/app/app-routing.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { CountriesComponent } from "./components/countries/countries.component";
 import { HomeComponent } from "./components/home/home.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   {
-    path: "",
+    path: "home",
     component: HomeComponent,
   },
   {
     path: "countries",
     component: CountriesComponent,
   },
+  // redirect to `first-component`
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  // Wildcard route for a 404 page
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
